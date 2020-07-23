2d63e7885f02d8bef1212d5a4db671a00d9eacc1
Brian <bedwards@atlassian.com>
2020-07-22 22:33:36 -0500

    improve suggestions

--- Data_Packages_User/sonnet.py
+++ Data_Packages_User/sonnet.py
@@ -1,3 +1,4 @@
+import random
 import re
 from collections import defaultdict
 from os.path import dirname, join
@@ -6,8 +7,7 @@ from sublime_plugin import ViewEventListener
 
 alternate = re.compile(r'.*\(\d\)$')
 
-bit_flags = 0
-# bit_flags = sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS
+completion_flags = sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS
 
 solid_underline = sublime.DRAW_SOLID_UNDERLINE | sublime.DRAW_NO_FILL | sublime.DRAW_NO_OUTLINE
 stippled_underline = sublime.DRAW_STIPPLED_UNDERLINE | sublime.DRAW_NO_FILL | sublime.DRAW_NO_OUTLINE
@@ -19,18 +19,50 @@ white = 'text'
 gray = 'comment'
 
 cmudict = {}
-completions = defaultdict(list)
+start_unstressed = defaultdict(lambda: defaultdict(list))
+start_stressed = defaultdict(lambda: defaultdict(list))
+
 for entry in open(join(dirname(__file__), 'cmudict-0.7b.txt')):
     if entry.startswith(';;;'):
         continue
+
     word, pronunciation = entry.rstrip().split(None, 1)
     word = word.lower()
     pronunciation = pronunciation.lower()
-    if alternate.match(word):
+    pronunciation = pronunciation.replace(' ', '-')
+
+    if '-' in word or alternate.match(word):
         continue
+
+    stress_markers = ''.join([s for s in pronunciation if s in '012'])
+
+    if (len(stress_markers) > 10
+            or '00' in stress_markers
+            or '11' in stress_markers
+            or '22' in stress_markers
+            or '12' in stress_markers
+            or '21' in stress_markers):
+        continue
+
     cmudict[word] = pronunciation
+
+    if stress_markers[0] == '0':
+        completions = start_unstressed
+    else:
+        completions = start_stressed
+
+    completions = completions[len(stress_markers)]
+    completions[''].append((word, word))
+
     for i, c in enumerate(word):
         completions[word[:i+1]].append((word, word))
+
+# for completions in start_unstressed, start_stressed:
+#     syllable_counts = list(completions.keys())
+#     syllable_counts.sort(reverse=True)
+#     for syllable_count in syllable_counts:
+#         print(syllable_count, len(completions[syllable_count]['']))
+
 
 def _cmudict_get(word):
     if word in cmudict:
@@ -56,7 +88,6 @@ def cmudict_get(word):
     pronunciation = _cmudict_get(word)
     if pronunciation is None:
         return '?', [(None, None)]
-    pronunciation = pronunciation.replace(' ', '-')
     stresses = [int(stress) for stress in pronunciation if stress in '012']
     return pronunciation, [(s, len(stresses)) for s in stresses]
 
@@ -80,16 +111,51 @@ class Sonnet(ViewEventListener):
     def __init__(self, view):
         super().__init__(view)
         self.meter_phantom_set = sublime.PhantomSet(view)
-        # self.rhyme_phantom_set = sublime.PhantomSet(view)
-        self.on_modified_async()
+        self.on_post_save()
 
     def on_query_completions(self, prefix, locations):
-        print(prefix, [self.view.rowcol(l) for l in locations])
-        replacements = completions[prefix.lower()][:7]
-        return replacements, bit_flags
+        # print(prefix == '', [self.view.rowcol(l) for l in locations])
 
-    def on_modified_async(self):
+        # line = self.view.line(locations[0])
+        # words = []
+        # for raw_word in self.view.substr(line).split():
+        #     words.extend(raw_word.lower().strip(',;:.?!').split('-'))
 
+        # [s for s in words[-1] if s in '012']
+
+        completions = start_unstressed
+        if self.view.rowcol(locations[0])[1] > 1:
+            prev_word_a = self.view.find_by_class(locations[0], False, sublime.CLASS_WORD_START | sublime.CLASS_LINE_START)
+            prev_word_b = self.view.find_by_class(prev_word_a, True, sublime.CLASS_WORD_END | sublime.CLASS_LINE_END)
+            prev_word = self.view.substr(sublime.Region(prev_word_a, prev_word_b))
+            prev_word = prev_word.lower().strip(',;:.?!').split('-')[-1]
+            prev_pronounce = cmudict.get(prev_word)
+            if prev_pronounce:
+                stress_markers = [s for s in prev_pronounce if s in '012']
+                if stress_markers[-1] == '0':
+                    completions = start_stressed
+
+        replacements = []
+        for syllable_count in 5, 4, 3, 2, 1:
+            replacements.append(random.choice(completions[syllable_count][prefix.lower()]))
+
+        return replacements, completion_flags
+
+        # contents = [ for word in contents.split()]
+
+        # [''.join([stress for stress in cmudict[word] if stress in '012'])
+        #     for word in contents.split()]
+
+        #     word = 
+        #         for subword in word.split('-'):
+        #             p, s = cmudict_get(subword)
+        #             pronunciations.append(p)
+        #             stresses.extend(s)
+
+        # replacements = completions[prefix.lower()][:7]
+        # return replacements, completion_flags
+
+    def on_post_save(self):
         line_contents = []
         longest_line = 0
 
@@ -168,6 +234,8 @@ class Sonnet(ViewEventListener):
             try:
                 line1, p1 = line_pronunciations[i]
                 line2, p2 = line_pronunciations[j]
+                p1[-1]
+                p2[-1]
             except IndexError:
                 continue
             if get_final_vowel_sound(p1[-1]) != get_final_vowel_sound(p2[-1]):
--- README.md
+++ README.md
@@ -1,1 +1,79 @@
 # sonnet
+
+## Single-syllable start unstressed
+
+Useful
+
+    a (ah0)
+    the (dh-ah0)
+    and (ah0-n-d)
+    'cause (k-ah0-z) because
+    in (ih0-n)
+    er (er0) used to express hesitation
+    hmm (hh-ah0-m) used typically to express thoughtful absorption, hesitation, doubt, or perplexity
+    hers (hh-er0-z) used to refer to a thing or things belonging to or associated with a female person or animal previously mentioned
+
+Junk
+
+    'em (ah0-m) them
+    'm (ah0-m) am
+    'n (ah0-n) than
+    chicked (ch-ih0-k-t)
+    gmbh (g-ah0-m) German phrase "Gesellschaft mit beschränkter Haftung" which means "company with limited liability"
+    hm (hh-ah0-m)
+    hye (hh-ay0) ???
+    in's (ih0-n-z) ???
+    le (l-ah0) ??? French?
+    mc (m-ih0-k) a miscarriage
+    whats (w-ah0-t-s) plural of noun what: the true nature or identity of something, or the sum of its characteristics: a lecture on the whats and hows of crop rotation
+    who've (hh-uw0-v)
+
+## Start unstressed count by syllable count
+
+Sonnet-worthy
+
+    7 4
+    6 9
+    5 782
+    4 604
+    3 12021
+    2 5698
+    1 20
+
+All
+
+    8 3
+    7 25
+    6 227
+    5 1258
+    4 5270
+    3 12691
+    2 5709
+    1 20
+
+## Start stressed count by syllable count
+
+Sonnet-worthy
+
+    8 1 (supercalifragilistic)
+    7 3
+    6 112
+    5 93
+    4 4816
+    3 5629
+    2 42845
+    1 16302
+
+All
+
+    14 1
+    12 1
+    9 2
+    8 12
+    7 87
+    6 483
+    5 2130
+    4 6915
+    3 21387
+    2 51589
+    1 16302
